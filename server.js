f837acb2cedb6928c10412b2fcdf377ebcd47b74
Sudeep Gangwar <gangwarsudeep7878@gmail.com>
2023-04-23 01:38:23 +0530

    changes

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const swaggerOptions = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "Vaidya Healthcare API endpoints",
+            title: "Vaidya Healthcare API endpoint",
             version: "1.0.0",
         },
         servers: [
